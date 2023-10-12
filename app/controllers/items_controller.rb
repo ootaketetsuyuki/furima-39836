e3883293096301c44947d1735be1def8015a1c55
ootaketetsuyuki <tetsuyukiotake@forthewin.co.jp>
2023-10-12 16:04:14 +0900

    商品情報編集機能実装修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
     redirect_to action: :index
     end
