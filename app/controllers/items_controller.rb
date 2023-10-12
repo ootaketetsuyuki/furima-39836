15b0779b428e10a778325ccaa07e44e64d8c4985
ootaketetsuyuki <tetsuyukiotake@forthewin.co.jp>
2023-10-12 15:19:53 +0900

    商品情報編集機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :find_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: :edit
+  
   def index
     @items = Item.order("created_at DESC")
   end
@@ -19,15 +21,16 @@ class ItemsController < ApplicationController
   end 
 
     def show
-      @item = Item.find(params[:id])
     end
 
     def edit
-      @item = Item.find(params[:id])
+      if user_signed_in? && current_user.id == @item.user_id
+      else
+        redirect_to action: :index
+      end
     end
 
     def update
-      @item = Item.find(params[:id])
       if @item.update(item_params)
         redirect_to item_path(@item.id)
       else
@@ -43,8 +46,14 @@ class ItemsController < ApplicationController
                                  :day_id, :price).merge(user_id: current_user.id)
   end
 
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    return if user_signed_in?
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
     redirect_to action: :index
+    end
   end
 end
